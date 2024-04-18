96d514b96c2aac99c32716c566d4c105a36c0c96
M0H1K4N <nejer911@gmail.com>
2024-04-18 23:06:44 +0400

    adding styled

--- src/components/Desktop/Info.jsx
+++ src/components/Desktop/Info.jsx
@@ -3,10 +3,14 @@ import React from "react";
 export default function Info() {
   return (
     <div className="infoDiv">
-      <h1></h1>
-      <input type="email" />
-      <button>Schedule a Demo</button>
-      <h4>Have any questions? Contact Us</h4>
+      <h1 className="infoH1">
+        Start building with our APIs for absolutely free.
+      </h1>
+      <div className="infoDivInputButton">
+        <input className="infoDivInput" placeholder="Enter email address" type="email" />
+        <button className="infoDivButton" >Schedule a Demo</button>
+      </div>
+      <h4 className="infoDivH4">Have any questions?  Contact Us</h4>
     </div>
   );
 }
