62663b881f611415801cf77278992be05487fa9b
M0H1K4N <nejer911@gmail.com>
2024-04-23 00:01:22 +0400

    fixing aboutPage

--- src/components/Desktop/AboutPage.jsx
+++ src/components/Desktop/AboutPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutPage() {
   return (
-    <div>
-        This is about page
-    </div>
-  )
+    <>
+      <div className="containerAbout">
+        <div className="containerAbout2">
+          <h1 className="aboutPageH1">
+            We empower innovators by delivering access to the financial system
+          </h1>
+          <div className="txtxAbout">
+            <div className="txtsAboutPage1">
+              <h2 className="aboutPageH2">Our Vision</h2>
+              <p className="aboutPageP">
+                Our main goal is to build beautiful consumer experiences along
+                with developer-friendly infrastructure. The result is an
+                intelligent tool that gives everyone the ability to create
+                amazing products that solve big problems. We are deeply focused
+                on democratizing financial services through technology.{" "}
+              </p>
+            </div>
+            <div className="txtsAboutPage1">
+              <h2 className="aboutPageH2">Our Business</h2>
+              <p className="aboutPageP">
+                At the core of our platform is the technical infrastructure APIs
+                that connect consumers. Our innovative product provides key
+                insights for businesses and individuals, as well as robust
+                reporting for traditional financial institutions and developers.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
